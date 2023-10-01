768a4ef46fa6dd6842e6b755fc4bdb5913a0545f
Fusaaaann <59491356+Fusaaaann@users.noreply.github.com>
2023-10-02 00:14:38 +0800

    modified prompt processing scripts

--- scripts/extractprompt.js
+++ scripts/extractprompt.js
@@ -1,4 +1,15 @@
 // 
-const extractPrompt = (content)=>console.log(content);
+const extractPrompt = (content)=>{
+
+    const regex = /### Your Prompt([\s\S]*?)```([\s\S]*?)```/i;
+    const match = content.match(regex);
+  
+    if (match && match.length >= 3) {
+      const codeBlockContent = match[2].trim();
+      console.log(codeBlockContent);
+    } else {
+      console.log(""); // Code block not found
+    }
+};
 
 extractPrompt(process.argv[1])
--- scripts/mergeprompt.js
+++ scripts/mergeprompt.js
@@ -1,4 +1,13 @@
 // TODO: insert backlink to let user locate the prompt and make pull request
-const mergePrompt = (base,modification)=>console.log(base+modification)
-
+const mergePrompt = (base,modification)=>{
+    const regex = /```([\s\S]*?)```/g;
+    const codeBlocks = [];
+    let match;
+  
+    while ((match = regex.exec(modification)) !== null) {
+      codeBlocks.push(match[1]);
+    }
+  
+    console.log(base+"\n"+codeBlocks.join('\n'));
+  }
 mergePrompt(process.argv[1],process.argv[2])// TEST: whitespace and quotation mark in argv[1] is escaped
