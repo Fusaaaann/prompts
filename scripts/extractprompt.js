a4c76551f02b748c62b26a1e54aa632d329397ff
Fusaaaann <59491356+Fusaaaann@users.noreply.github.com>
2023-10-02 10:11:49 +0800

    modfiy prompt extracting logic

--- scripts/extractprompt.js
+++ scripts/extractprompt.js
@@ -1,11 +1,11 @@
 // 
 const extractPrompt = (content)=>{
 
-    const regex = /### Your Prompt([\s\S]*?)```([\s\S]*?)```/i;
+    const regex = /### Your Prompt[\s\S]*```txt\n([\s\S]*?)```/i;
     const match = content.match(regex);
   
-    if (match && match.length >= 3) {
-      const codeBlockContent = match[2].trim();
+    if (match && match.length >= 2) {
+      const codeBlockContent = match[1].trim();
       console.log(codeBlockContent);
     } else {
       console.log(""); // Code block not found
