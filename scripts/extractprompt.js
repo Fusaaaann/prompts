2b6abea011ae615ff0100690ed061a515a64234e
Fusaaaann <59491356+Fusaaaann@users.noreply.github.com>
2023-10-02 10:28:50 +0800

    debug

--- scripts/extractprompt.js
+++ scripts/extractprompt.js
@@ -3,6 +3,7 @@ const extractPrompt = (content)=>{
 
     const regex = /### Your Prompt[\s\S]*```txt\n([\s\S]*?)```/i;
     const match = content.match(regex);
+    console.debug("content",content);
   
     if (match && match.length >= 2) {
       const codeBlockContent = match[1].trim();
